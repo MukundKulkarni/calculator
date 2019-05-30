00f83b473c32149f87e7f2d600fca2a8d1685f31
MukundKulkarni <mukundkulkarni786@gamil.com>
2019-05-30 15:12:34 +0530

    Added Multiplication and Divison

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class ExampleApp(QtWidgets.QMainWindow, calculator.Ui_Dialog):
         super(ExampleApp, self).__init__(parent)
         self.setupUi(self)
         self.Add.clicked.connect(self.add)
+        self.Sub.clicked.connect(self.sub)
+        self.Mul.clicked.connect(self.mul)
+        self.Div.clicked.connect(self.div)
         
     def clear(self):
         self.input_1.setText("")
@@ -20,6 +23,32 @@ class ExampleApp(QtWidgets.QMainWindow, calculator.Ui_Dialog):
        sum = num1 + num2
        self.result.setText("%.2f" % sum)
        self.clear()
+       
+    def sub(self):
+        num1 = float(self.input_1.text())
+        num2 = float(self.input_2.text())
+        sub = num1 - num2
+        self.result.setText("%.2f" % sub)
+        self.clear()
+        
+    def mul(self):
+        num1 = float(self.input_1.text())
+        num2 = float(self.input_2.text())
+        mul = num1 * num2
+        self.result.setText("%.2f" % mul)
+        self.clear()
+    
+    def div(self):
+        num1 = float(self.input_1.text())
+        num2 = float(self.input_2.text())
+        if num2 == 0:
+            self.result.setText("Error Division by ZERO !!!")
+        div = num1 / num2
+        self.result.setText("%.2f" % div)
+        self.clear()
+    
+    
+        
     
 def main():
     app = QApplication(sys.argv)
